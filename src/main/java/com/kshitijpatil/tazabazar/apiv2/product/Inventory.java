7293fdc6846b0b0a97e4980733222a12704c05f3
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-16 16:53:58 +0530

    Minor refactor
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- src/main/java/com/kshitijpatil/tazabazar/apiv2/product/Inventory.java
+++ src/main/java/com/kshitijpatil/tazabazar/apiv2/product/Inventory.java
@@ -1,18 +1,17 @@
 package com.kshitijpatil.tazabazar.apiv2.product;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
+@AllArgsConstructor(onConstructor_ = @PersistenceConstructor)
 public class Inventory {
-    @Transient
-    private static final int PRICE_SCALE = 2;
     @Id
     public Long id;
     public AggregateReference<Product, String> productSku;
@@ -20,16 +19,6 @@ public class Inventory {
     public BigDecimal price;
     public Instant updatedAt;
     public Integer stockAvailable;
-
-    @PersistenceConstructor
-    public Inventory(Long id, AggregateReference<Product, String> productSku, String quantityLabel, BigDecimal price, Instant updatedAt, Integer stockAvailable) {
-        this.id = id;
-        this.productSku = productSku;
-        this.quantityLabel = quantityLabel;
-        this.price = price;
-        this.updatedAt = updatedAt;
-        this.stockAvailable = stockAvailable;
-    }
 
     public Inventory(String quantityLabel, double price, Instant updatedAt, Integer stockAvailable) {
         this.quantityLabel = quantityLabel;
