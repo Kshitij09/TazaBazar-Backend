c1b28c65504fe119771be1a40cb60d3c6c205e29
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-17 12:56:39 +0530

    Add default constructor
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- src/main/java/com/kshitijpatil/tazabazar/ApiErrorResponse.java
+++ src/main/java/com/kshitijpatil/tazabazar/ApiErrorResponse.java
@@ -2,11 +2,13 @@ package com.kshitijpatil.tazabazar;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class ApiErrorResponse {
     private OffsetDateTime timestamp;
     private String error;
