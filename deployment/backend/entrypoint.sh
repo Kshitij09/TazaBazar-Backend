b1359b636d75bf4ffa194a3817128f363eb93da7
Kshitij Patil <kshitijpatil98@gmail.com>
2021-09-21 21:14:18 +0530

    Use 'prod' profile for deployment
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- deployment/backend/entrypoint.sh
+++ deployment/backend/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /wait && exec java -cp  app:app/lib/* ${JAVA_OPTS} \
- com.kshitijpatil.tazabazar.ApiServerApplication ${@}
+ com.kshitijpatil.tazabazar.ApiServerApplication ${@} \
+ --spring.profiles.active=prod
