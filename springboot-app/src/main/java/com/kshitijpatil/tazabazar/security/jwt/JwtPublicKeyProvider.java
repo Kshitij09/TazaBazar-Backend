a345ea700d4bb8108fa1ccbe22885937e318939c
Kshitij Patil <kshitijpatil98@gmail.com>
2021-11-09 01:56:48 +0530

    Read public key from X509 Certificate
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- springboot-app/src/main/java/com/kshitijpatil/tazabazar/security/jwt/JwtPublicKeyProvider.java
+++ springboot-app/src/main/java/com/kshitijpatil/tazabazar/security/jwt/JwtPublicKeyProvider.java
@@ -1,48 +1,39 @@
 package com.kshitijpatil.tazabazar.security.jwt;
 
-import com.kshitijpatil.tazabazar.utils.Base64Util;
-import com.kshitijpatil.tazabazar.utils.ReadKeyMixin;
+import com.kshitijpatil.tazabazar.configuration.JwtConfig;
 import com.kshitijpatil.tazabazar.utils.ResourceUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.security.KeyFactory;
+import java.io.IOException;
 import java.security.PublicKey;
-import java.security.spec.EncodedKeySpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 @Component
 @RequiredArgsConstructor
-public class JwtPublicKeyProvider implements ReadKeyMixin {
+public class JwtPublicKeyProvider {
     @Getter
     private final ResourceUtil resourceUtil;
-    private final Base64Util base64Util;
-
-    @Getter
+    private final JwtConfig jwtConfig;
     private PublicKey publicKey;
 
     @PostConstruct
     public void init() {
-        publicKey = readKey(
-                "classpath:keys/tzb_key.x509.public",
-                "PUBLIC",
-                this::publicKeySpec,
-                this::publicKeyGenerator
-        );
-    }
-
-    private EncodedKeySpec publicKeySpec(String data) {
-        return new X509EncodedKeySpec(base64Util.decode(data));
-    }
-
-    private PublicKey publicKeyGenerator(KeyFactory kf, EncodedKeySpec spec) {
         try {
-            return kf.generatePublic(spec);
-        } catch (InvalidKeySpecException e) {
-            throw new JwtInitializationException(e);
+            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+            var keyInputStream = resourceUtil.getInputStream(jwtConfig.getPublicKeyFilepath());
+            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(keyInputStream);
+            publicKey = cert.getPublicKey();
+        } catch (CertificateException | IOException ex) {
+            throw new JwtInitializationException(ex);
         }
+    }
+
+    public PublicKey get() {
+        return publicKey;
     }
 }
