14b442b0d8633894263184b4837cbe615ef296d6
Kshitij Patil <kshitijpatil98@gmail.com>
2021-11-09 01:57:42 +0530

    Update error message
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- springboot-app/src/main/java/com/kshitijpatil/tazabazar/security/jwt/JwtInitializationException.java
+++ springboot-app/src/main/java/com/kshitijpatil/tazabazar/security/jwt/JwtInitializationException.java
@@ -2,6 +2,6 @@ package com.kshitijpatil.tazabazar.security.jwt;
 
 public class JwtInitializationException extends RuntimeException {
     public JwtInitializationException(Throwable e) {
-        super("Something went wong while reading private key!", e);
+        super("Error initializing public/private key!", e);
     }
 }
